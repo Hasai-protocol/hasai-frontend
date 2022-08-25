6ee92c3c43b58835908c9a2fe3281b2b62b20c82
shirleyKush <shirleyKush@proton.me>
2022-08-25 12:17:28 +0800

    Update index.tsx

--- src/view/Home/index.tsx
+++ src/view/Home/index.tsx
@@ -103,7 +103,7 @@ export default observer(function Home() {
                             >
                                 <Radio.Button value={-1}>All</Radio.Button>
                                 <Radio.Button value={1}>Blue Chip</Radio.Button>
-                                <Radio.Button value={2}>Share</Radio.Button>
+                                <Radio.Button value={2}>Shared</Radio.Button>
                                 <Radio.Button value={0}>
                                     Permissionless
                                 </Radio.Button>
