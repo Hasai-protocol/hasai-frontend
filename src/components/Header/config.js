5630a4dbfd3642bcba6540d6fde591ff2996e143
shirleyKush <shirleyKush@proton.me>
2022-11-18 12:01:58 +0800

    feat:remove trash

--- src/components/Header/config.js
+++ src/components/Header/config.js
@@ -5,18 +5,18 @@ import github from "../../asset/header/github.png";
 import explorer from "../../asset/header/explorer.png";
 import exit from "../../asset/header/exit.png";
 export const homeTabs = [
-    {
-        TabName: "Hasai Protocol",
-        path: "https://github.com/Hasai-protocol",
-    },
-    {
-        TabName: "Docs",
-        path: "https://docs.hasai.xyz/",
-    },
-    {
-        TabName: "Security",
-        path: "https://docs.hasai.xyz/risk/security-and-audits",
-    },
+    // {
+    //     TabName: "Hasai Protocol",
+    //     path: "https://github.com/Hasai-protocol",
+    // },
+    // {
+    //     TabName: "Docs",
+    //     path: "https://docs.hasai.xyz/",
+    // },
+    // {
+    //     TabName: "Security",
+    //     path: "https://docs.hasai.xyz/risk/security-and-audits",
+    // },
 ];
 export const appTabs = [
     {
@@ -29,24 +29,24 @@ export const appTabs = [
         active: "markets",
         path: "/markets/0",
     },
-    {
-        active: "null",
-        TabName: "Governance",
-        path: "https://docs.hasai.xyz/governance/governance-mechanism",
-    },
+    // {
+    //     active: "null",
+    //     TabName: "Governance",
+    //     path: "https://docs.hasai.xyz/governance/governance-mechanism",
+    // },
 ];
 
 export const MoreList = [
-    {
-        title: "Discord",
-        link: `https://discord.com/invite/tbnz5jfyYa`,
-        img: discord,
-    },
-    {
-        title: "Github",
-        link: `https://github.com/orgs/Hasai-protocol/repositories`,
-        img: github,
-    },
+    // {
+    // title: "Discord",
+    //     link: `https://discord.com/invite/tbnz5jfyYa`,
+    //     img: discord,
+    // },
+    // {
+    //     title: "Github",
+    //     link: `https://github.com/orgs/Hasai-protocol/repositories`,
+    //     img: github,
+    // },
     {
         title: "Creat Permissionless Pools",
         link: `/addPool`,
@@ -58,11 +58,11 @@ export const AccountFunc = [
         title: "Copy Address",
         img: copy,
     },
-    {
-        title: "Veiw on Explorer",
-        link: `https://github.com/orgs/Hasai-protocol/repositories`,
-        img: explorer,
-    },
+    // {
+    //     title: "Veiw on Explorer",
+    //     link: `https://github.com/orgs/Hasai-protocol/repositories`,
+    //     img: explorer,
+    // },
     {
         title: "Disconnect Wallet",
         link: `/addPool`,
